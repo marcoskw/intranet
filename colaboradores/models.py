b68c2de6e00b55e115bbb2731cba4ec0a7bcee64
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-02-02 15:27:33 -0300

    Criação do Banco de Dados e Model (Colaborador e Setor)

--- colaboradores/models.py
+++ colaboradores/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Setor(models.Model):
+    setor = models.CharField(max_length=255)
+
+
+class Colaborador(models.Model):
+    nome_completo = models.CharField(max_length=255)
+    setor = models.CharField(max_length=255)
+    ramal = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_admissao = models.DateTimeField(default=timezone.now, blank=True)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Setor, on_delete=models.DO_NOTHING)
