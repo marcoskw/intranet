0566ee9262b54e2da6c9836177df71ba475f5f25
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-02-03 11:47:02 -0300

    Criação da Parte Administrativa para a parte de criação de colaboradores e setores

--- colaboradores/admin.py
+++ colaboradores/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import Setor, Colaborador
 
-# Register your models here.
+
+class ColaboradorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_completo', 'setor', 'ramal', 'email')
+    list_display_links = ('id', 'nome_completo')
+    list_filter = ('setor', )
+    search_fields = ('nome_completo', 'setor', 'ramal', 'email', 'aniversario')
+
+
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'setor')
+
+
+admin.site.register(Colaborador, ColaboradorAdmin)
+admin.site.register(Setor, SetorAdmin)
+
+
--- colaboradores/models.py
+++ colaboradores/models.py
@@ -5,13 +5,20 @@ from django.utils import timezone
 class Setor(models.Model):
     setor = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.setor
+
 
 class Colaborador(models.Model):
     nome_completo = models.CharField(max_length=255)
-    setor = models.CharField(max_length=255)
+    setor = models.ForeignKey(Setor, on_delete=models.DO_NOTHING)
     ramal = models.CharField(max_length=255, blank=True)
     email = models.CharField(max_length=255, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     data_admissao = models.DateTimeField(default=timezone.now, blank=True)
+    aniversario = models.CharField(max_length=5, blank=True)
     descricao = models.TextField(blank=True)
-    categoria = models.ForeignKey(Setor, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome_completo
+
