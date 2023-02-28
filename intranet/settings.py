6fa3983259d6cafe0508f8da90107a7780a4d112
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-02-28 16:56:09 -0300

    Ajuste da pasta e arquivos 'static' do projeto

--- intranet/settings.py
+++ intranet/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
+STATIC_ROOT = os.path.join('static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
