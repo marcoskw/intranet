35bee855cfacecacb4a14348768305202917e366
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-03-06 16:20:27 -0300

    Implantando a Página Inicial dos Posts

--- intranet/settings.py
+++ intranet/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'colaboradores.apps.ColaboradoresConfig',
+    'django.contrib.humanize',
     'django_summernote',
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,8 @@ from .models import Post
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
+    paginate_by = 3
+    context_object_name = 'posts'
 
 
 class PostBusca(PostIndex):
