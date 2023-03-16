b4f7c172174c2356cfbf7054c2f1d694c67453f6
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2023-03-16 17:19:18 -0300

    Adicionando Detalhes do Post e Adição de comentários

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
 from django.db.models import Q, Count, Case, When
+from comentarios.forms import FormComentario
+from comentarios.models import Comentario
 
 
 class PostIndex(ListView):
@@ -61,4 +64,20 @@ class PostCategoria(PostIndex):
 
 
 class PostDetalhes(UpdateView):
-    pass
+    template_name = 'posts/post_detalhes.html'
+    model = Post
+    form_class = FormComentario
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = self.get_object()
+        comentario = Comentario(**form.cleaned_data)
+        comentario.post_comentario = post
+
+        # if self.request.user.is_autenticated:
+        #    comentario.usuario_comentario = self.request.user
+
+        comentario.save()
+        messages.success(self.request, 'Comentário enviado para análise com Sucesso!')
+
+        return redirect('post_detalhes', pk=post.id)
